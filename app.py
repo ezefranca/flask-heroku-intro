1e141c7d574d997b4ec2883a8ccaa238a53f6dca
Ezequiel França dos Santos <ezefranca@users.noreply.github.com>
2016-03-12 11:31:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 import os
-from flask import Flask
+import flask
 
-# initialization
-app = Flask(__name__)
-app.config.update(
-    DEBUG = True,
-)
 
-# controllers
-@app.route("/<name>")
+# Create the application.
+APP = flask.Flask(__name__)
+
+
+@APP.route('/')
+def index():
+    """ Displays the index page accessible at '/'
+    """
+    return flask.render_template('index.html')
+
+
+if __name__ == '__main__':
+    APP.debug=True
+    APP.run()
+
+@APP.route('/hello/<name>/')
 def hello(name):
+    """ Displays the page greats who ever comes to visit it.
+    """
     return flask.render_template('hello.html', name=name)
-    #return "Hello from Python!"
-
-# launch
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-#    return flask.render_template('hello.html')
-
-
-

b5271bf2f689eb9c5a4cf3c7f11e7141af576667
Ezequiel França dos Santos <ezefranca@users.noreply.github.com>
2016-03-12 10:51:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask
-#from twython import TwythonStreamer
 
 # initialization
 app = Flask(__name__)
@@ -9,19 +8,15 @@ app.config.update(
 )
 
 # controllers
-@app.route('/')
+@app.route("/")
 def hello():
-    return flask.render_template('hello.html')
-    #return "Hello from Python!"
-#@app.route("/")
-#def hello():
-#    print "teste"
-#    return "Hello from Python!"
+    return "Hello from Python!"
 
 # launch
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+#    return flask.render_template('hello.html')
 
 
 
